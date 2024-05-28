5a7f68720599914418c00dbfd23f96e52e053c3c
remijul <remi.julien@dcid-consulting.fr>
2024-05-28 18:28:36 +0200

    Initial commit

---
+++ test_scripts_pour_tests.py
@@ -0,0 +1,8 @@
+from scripts_pour_tests import reverse_str_ko, reverse_str_ok
+
+def test_should_reverse_string():
+    assert reverse_str_ko('abc') == 'cba', 'Error in reversing string'
+    
+def test_should_reverse_string2():    
+    assert reverse_str_ok('abc') == 'cba', 'Error in reversing string'
+
